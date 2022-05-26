9934764bca6879fdf1c162b11f0b1fb063a88c37
Pierluigi <dev-pierluigi>
2022-05-26 10:09:58 +0200

    DONE: Aggiunto Deploy in DETA e .env

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+from fastapi import FastAPI, Request, requests
+import os
+from dotenv import load_dotenv
+import httpx
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+app = FastAPI()
+
+load_dotenv()
+
+GROUP_ID =  os.getenv('CHAT_ID')
+BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+PROJECT_NAME = os.getenv('PROJECT_NAME')
+
+
+
+@app.get("/{name}")
+async def helloname(name:str):
+    """
+    Returns a Hello to the User with a wave emoji
+    """
+    """"""
+    return f"Hello {GROUP_ID} 👋"
+
+
+async def sendTgMessage(message: str):
+    """
+    Sends the Message to telegram with the Telegram BOT API
+    """
+    tg_msg = {"chat_id": GROUP_ID, "text": message, "parse_mode": "Markdown"}
+    API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    async with httpx.AsyncClient() as client:
+     result = await client.post(API_URL, data = tg_msg)
+     return result.json()
+
+@app.post("/hook")
+async def recWebHook(req: Request):
+    """
+    Receive the Webhook and process the Webhook Payload to get relevant data
+    Refer https://developer.github.com/webhooks/event-payloads for all GitHub Webhook Events and Payloads
+    """
+    body = await req.json()
+    print(body)
+    event = req.headers.get("X-Github-Event")
+    if event == "push":  # check if the event is a star
+        ref = body["ref"]
+        username = body["pusher"]["name"]
+        msg = body["commits"][0]["message"]
+        message = f"{username} ha pushato un aggiornamento sul progetto! \n *{PROJECT_NAME}*  \n sul branch: {ref} \n con il messaggio: *{msg}* \n"
+        await sendTgMessage(message=message)
+     
+        
+
+
+
+
